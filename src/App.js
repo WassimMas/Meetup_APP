19bf76110a353b9a1c57fb90904145d854535178
wassimmastour <wassim.mastour@outlook.com>
2023-10-12 13:45:09 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetupPage from "./pages/NewMeetup";
+import FavoritesPage from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<AllMeetupsPage />} />
+        <Route path="/new-meetup" element={<NewMeetupPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
