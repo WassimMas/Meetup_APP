39214836f840126823e7e5bbda55a494716d0e5a
wassimmastour <wassim.mastour@outlook.com>
2023-10-16 14:40:32 +0100

    fetching data

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,27 @@
+import { useNavigate } from "react-router-dom";
+
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+  const navigate = useNavigate();
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-getting-started-5923c-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "content-type": "application/json",
+        },
+      }
+    ).then(() => {
+      navigate("/");
+    });
+  }
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
